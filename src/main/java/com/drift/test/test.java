b4ae5a27f94771ed51df3b9094cb42522aa65369
zxy2chm1314 <927265436@qq.com>
2020-08-28 22:44:58 +0800

    Initial commit

---
+++ src/main/java/com/drift/test/test.java
@@ -0,0 +1,9 @@
+package com.drift.test;/**
+ * User: "周星谕"
+ * DateTime: 2020/8/28  22:29
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("git test 测试");
+    }
+}
