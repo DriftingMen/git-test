be88259dcb9abb070af9479d2692d143dfd237ce
zxy2chm1314 <927265436@qq.com>
2020-08-28 23:13:36 +0800

    够狠够狠就

---
+++ src/main/java/com/drift/test/test1.java
@@ -0,0 +1,13 @@
+package com.drift.test;
+
+/**
+ * User: "周星谕"
+ * DateTime: 2020/8/28  22:29
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("git test 测试");
+        System.out.println("git test 测试");
+        System.out.println("git test 测试");
+    }
+}
